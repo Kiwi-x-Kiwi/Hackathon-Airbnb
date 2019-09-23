ad03343635d58c95c6d76466e1c5a8ed9e210cc6
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-23 16:41:05 -0400

    pulling

--- models/home.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const homeSchema = new Schema({
-    name: String,
-    description: String,
-    location: { type: { type: String }, coordinates: [Number] }
-});
-homeSchema.index({ location: '2dsphere' });
--- routes/details.js
+++ routes/details.js
@@ -4,7 +4,7 @@ const home = require('../models/home')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-    res.render('details/details');
+    res.render('details/new');
 });
 
 
